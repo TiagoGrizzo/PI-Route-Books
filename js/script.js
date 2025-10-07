83c41e3b86a617fb065b706385f2411cdf38949e
Lucas Eduardo Pereira da Cruz <lucascruzjau@gmail.com>
2025-10-06 21:58:54 -0300

    Script para mudar a foto de perfil

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//Script para mudar foto de perfil 
+  const inputFoto = document.getElementById('input-foto');
+  const fotoPreview = document.getElementById('foto-preview');
+
+  inputFoto.addEventListener('change', function() {
+    const file = this.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function(e) {
+        fotoPreview.src = e.target.result; // Atualiza a foto de pefil
+      };
+      reader.readAsDataURL(file);
+    }
+  });
+
