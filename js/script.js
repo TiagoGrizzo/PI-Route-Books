7406b712d2ec5ce2a7fdb249e57b89de4b349f28
Lucas Eduardo Pereira da Cruz <lucascruzjau@gmail.com>
2025-10-27 20:54:58 -0300

    Ajustando detalhes, dark-mode, design, etc...

--- js/script.js
+++ js/script.js
@@ -1,21 +1,35 @@
 //Script para mudar foto de perfil 
-  const inputFoto = document.getElementById('input-foto');
-  const fotoPreview = document.getElementById('foto-preview');
+ const btnFoto = document.getElementById('btn-trocar-foto');
+    const inputFoto = document.getElementById('input-foto');
+    const fotoPreview = document.getElementById('foto-preview');
 
-  inputFoto.addEventListener('change', function() {
-    const file = this.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = function(e) {
-        fotoPreview.src = e.target.result; // Atualiza a foto de pefil
-      };
-      reader.readAsDataURL(file);
-    }
-  });
+    // Abre o seletor de arquivos ao clicar no botão
+    btnFoto.addEventListener('click', () => {
+        inputFoto.click();
+    });
 
+    // Atualiza a foto de perfil
+    inputFoto.addEventListener('change', function() {
+        const file = this.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                fotoPreview.src = e.target.result;
+            };
+            reader.readAsDataURL(file);
+        }
+    });
 
   
   /* Dark Mode */
-  function toggleDarkMode() {
-    document.body.classList.toggle('dark-mode');
+function toggleDarkMode() {
+  document.body.classList.toggle('dark-mode');
+
+  const logo = document.getElementById('logoSobre');
+
+  if (document.body.classList.contains('dark-mode')) {
+    logo.src = 'imgs/rb-logo2.png'; // logo com escrita branca
+  } else {
+    logo.src = 'imgs/rb-logo1.png'; // logo com escrita preta
   }
+}
